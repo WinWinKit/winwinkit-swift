76007fdb86a39ea3f7787aba5e7b218dc83bc9e5
Oleh <oleh.stasula@gmail.com>
2024-12-04 15:32:04 +0100

    Defined initial ReferralUser model

---
+++ Sources/WinWinKit/ReferralUser.swift
@@ -0,0 +1,16 @@
+//
+//  Copyright WinWinKit. All Rights Reserved.
+//
+//  Licensed under the MIT License (the "License").
+//  You may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at https://opensource.org/licenses/MIT
+//
+//  ReferralUser.swift
+//
+//  Created by Oleh Stasula on 04/12/2024.
+//
+
+public struct ReferralUser: Codable {
+    public let userId: String
+    public let code: String?
+}
