88e86ebd88bee8e1ac7c2a1d656cec09f21e8a9d
Oleh <oleh.stasula@gmail.com>
2024-12-18 13:32:03 +0100

    Minor tests structure cleanup

--- Tests/WinWinKitTests/WinWinKitTests.swift
+++ Tests/WinWinKitTests/WinWinKitTests.swift
@@ -1,6 +1,8 @@
 import Testing
 @testable import WinWinKit
 
-@Test func example() async throws {
-    // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+@Suite struct WinWinKitTests {
+    
+    @Test func configure() async throws {
+    }
 }
