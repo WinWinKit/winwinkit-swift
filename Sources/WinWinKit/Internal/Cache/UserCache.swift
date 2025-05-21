b6489b8dace452f15e9a459c0bdde01c8153399a
Oleh <oleh.stasula@gmail.com>
2025-05-21 10:21:15 +0200

    Refactored UserCache to struct type #1

--- Sources/WinWinKit/Internal/Cache/UserCache.swift
+++ Sources/WinWinKit/Internal/Cache/UserCache.swift
@@ -10,71 +10,69 @@
 //  Created by Oleh Stasula on 14/12/2024.
 //
 
-protocol UserCacheType: AnyObject {
-    var user: User? { get set }
-    var userUpdate: UserUpdate? { get set }
+protocol UserCacheType {
+    var user: User? { get nonmutating set }
+    var userUpdate: UserUpdate? { get nonmutating set }
 }
 
 extension UserCacheType {
-    
     func reset() {
         self.user = nil
         self.userUpdate = nil
     }
 }
 
-final class UserCache: UserCacheType {
-    
+struct UserCache: UserCacheType {
     let keyValueCache: KeyValueCacheType
-    
+
     init(keyValueCache: KeyValueCacheType) {
         self.keyValueCache = keyValueCache
     }
-    
+
     // MARK: - UserCacheType
-    
+
     var user: User? {
         get {
             do {
                 return try self.keyValueCache[Keys.user].map { try User(jsonData: $0) }
             }
             catch {
-                Logger.error("Unable to deserialize ReferralUser.")
+                Logger.error("Unable to deserialize User.")
                 return nil
             }
         }
-        set {
+        nonmutating set {
             do {
                 self.keyValueCache[Keys.user] = try newValue?.jsonData()
             }
             catch {
-                Logger.error("Unable to serialize ReferralUser.")
+                Logger.error("Unable to serialize User.")
             }
         }
     }
-    
+
     var userUpdate: UserUpdate? {
         get {
             do {
                 return try self.keyValueCache[Keys.userUpdate].map { try UserUpdate(jsonData: $0) }
             }
             catch {
-                Logger.error("Unable to deserialize update ReferralUser.")
+                Logger.error("Unable to deserialize update UserUpdate.")
                 return nil
             }
         }
-        set {
+        nonmutating set {
             do {
                 self.keyValueCache[Keys.userUpdate] = try newValue?.jsonData()
             }
             catch {
-                Logger.error("Unable to serialize update ReferralUser.")
+                Logger.error("Unable to serialize update UserUpdate.")
             }
         }
     }
-    
+
     // MARK: - Private
-    
+
     private enum Keys {
         static let user = "com.winwinkit.cache.user"
         static let userUpdate = "com.winwinkit.cache.userUpdate"
